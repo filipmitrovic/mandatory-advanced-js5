f455b871c8b5906bdc881fbbe5f6ac5ca8b54021
Hesham Alkurdi <alkurdi.hesham@gmail.com>
2019-05-07 10:46:47 +0200

    adding filelist test

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,15 +2,31 @@ import React, { Component } from "react";
 import { Link, Redirect } from "react-router-dom";
 import axios from "axios";
 import { Helmet } from "react-helmet";
+import { Dropbox } from 'dropbox';
+
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      filelist:""
     }; 
   }
-
+  componentDidMount(){
+    const accessToken = localStorage.token;
+    console.log(accessToken)
+    const dbx = new Dropbox({
+      accessToken,
+      fetch
+    });
+    dbx.filesListFolder({
+      path: ''
+    }).then(
+      response => {
+        this.setState({filelist:response})
+        console.log(this.state.filelist)
+      })
+  }
   render() {
     return (
       <>
